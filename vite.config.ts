111fa3465231643e439f7498144458e74ea216c1
sekema954 <sekema954@gmail.com>
2025-05-22 17:05:07 -0400

    inital commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://sekema954.github.io/jadoo/',
+  base: '/jadoo/',
   plugins: [
     react(),
     tailwindcss(),
